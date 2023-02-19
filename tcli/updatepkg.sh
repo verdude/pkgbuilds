fef88c63ba18101163a1f09297ed334652a64766
Santiago Verdu <erra@qmake.org>
2023-02-18 19:06:17 -0700

    Some options for update script

--- tcli/updatepkg.sh
+++ tcli/updatepkg.sh
@@ -2,17 +2,11 @@
 
 set -euo pipefail
 
-stashed=""
-install=""
+declare -r myname="updatepkg.sh"
+declare -i skipcleanup=0 runstash=0
+declare install="" stashed=""
 
-while getopts i flag
-do
-  case ${flag} in
-    i) install="i";;
-  esac
-done
-
-stash() {
+function stash() {
   set +e
   git diff-files --quiet
   result=$?
@@ -24,7 +18,7 @@ stash() {
   fi
 }
 
-pop() {
+function pop() {
   if [[ -n "$stashed" ]]; then
     git stash pop
     git reset
@@ -32,21 +26,52 @@ pop() {
 }
 
 function exit() {
+  (( skipcleanup )) && return
   git clean -dff
-  pop
+  (( runstash )) && pop
 }
 trap exit EXIT
 
-stash
-makepkg -cCsrf$install
+function usage() {
+  cat << EOF
+PKGBUILD builder/installer.
+
+Usage: bash ${myname} [options]
+
+Options:
+  -i    install package after build.
+  -s    skip cleanup after build.
+  -r    stash changes in this repo before starting.
+EOF
+
+  exit
+}
+
+while getopts :isrh flag; do
+  case ${flag} in
+    i) install="i" ;;
+    s) skipcleanup=1 ;;
+    r) runstash=1 ;;
+    h) usage ;;
+    :) ;;
+    ?) ;;
+  esac
+done
+
+(( runstash )) && stash
+makepkg -cCsrf${install}
 set +e
 git diff-files --quiet
 result=$?
 set -e
 
 if [[ $result -eq 1 ]]; then
-  . PKGBUILD
+  if [[ ! -f PKGBUILD ]]; then
+    echo "PKGBUILD not found."
+    exit 1
+  fi
+  source PKGBUILD
   git add ./PKGBUILD
-  git commit -m "$(basename $(pwd)) ${pkgver}"
+  git commit -m "$(basename "$(pwd)") ${pkgver}"
   git push
 fi
